e8e9dc4d8cd00f7b27aae1d073f3ee785d51eead
MarioElProgramador <mario.valenzuela01@estudiant.upf.edu>
2024-05-06 22:10:45 +0200

    Pagina "Seleccionar curso" creada
    Ahora se pueden seleccionar los cursos y van a actualizarse en la variable correspondiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,60 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.red, // Cambia el color de la barra de navegación
+        primaryColor: Colors.red,
       ),
-      home: PaginaPrincipal(), // Usamos la clase PaginaPrincipal como la página principal
+      home: PaginaPrincipal(),
     );
   }
 }
 
-class PaginaPrincipal extends StatelessWidget {
+class PaginaPrincipal extends StatefulWidget {
+  @override
+  _PaginaPrincipalState createState() => _PaginaPrincipalState();
+}
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  int _contadorFuego = 0;
+  DateTime _ultimaConexion = DateTime.now();
+  bool _fuegoEncendido = false;
+  String _curso = "Curso";
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarDatos();
+  }
+
+  Future<void> _cargarDatos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _contadorFuego = prefs.getInt('contadorFuego') ?? 0;
+      _ultimaConexion = DateTime.parse(prefs.getString('ultimaConexion') ?? DateTime.now().toString());
+      _fuegoEncendido = _esFuegoEncendido(_ultimaConexion);
+      _curso = prefs.getString('curso') ?? "Curso";
+    });
+    _verificarActualizacion();
+  }
+
+  bool _esFuegoEncendido(DateTime fecha) {
+    DateTime ahora = DateTime.now();
+    Duration diferencia = ahora.difference(fecha);
+    return diferencia.inDays >= 1;
+  }
+
+  Future<void> _verificarActualizacion() async {
+    DateTime ahora = DateTime.now();
+    if (ahora.day != _ultimaConexion.day || ahora.month != _ultimaConexion.month || ahora.year != _ultimaConexion.year) {
+      setState(() {
+        _contadorFuego++;
+        _ultimaConexion = ahora;
+        _fuegoEncendido = true;
+      });
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('contadorFuego', _contadorFuego);
+      await prefs.setString('ultimaConexion', _ultimaConexion.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,43 +71,56 @@ class PaginaPrincipal extends StatelessWidget {
       appBar: AppBar(
         title: Row(
           children: [
-            Icon(Icons.school), // Icono de la materia/asignatura seleccionada
-            SizedBox(width: 8.0), // Espacio entre el icono y el siguiente elemento
-            Expanded(
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.whatshot), // Icono del fuego apagado
-                    SizedBox(width: 4.0), // Espacio entre el icono y el número de días
-                    Text(
-                      '0', // Número de días consecutivos (por defecto 0)
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+            GestureDetector(
+              onTap: () async {
+                final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => SeleccionCurso()));
+                if (result != null) {
+                  setState(() {
+                    _curso = result;
+                  });
+                }
+              },
+              child: Icon(Icons.school),
+            ),
+            SizedBox(width: 8.0),
+            GestureDetector(
+              onTap: () async {
+                final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => SeleccionCurso()));
+                if (result != null) {
+                  setState(() {
+                    _curso = result;
+                  });
+                }
+              },
+              child: Text(
+                '$_curso',
+                style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
               ),
             ),
-            SizedBox(width: 8.0), // Espacio entre el siguiente elemento y el icono de la gema
-            Row(
-              children: [
-                Icon(Icons.attach_money), // Icono del dinero
-                SizedBox(width: 4.0), // Espacio entre el icono y el número de monedas
-                Text(
-                  '0', // Número de monedas del usuario (por defecto 0)
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+            Spacer(),
+            _fuegoEncendido
+                ? Icon(Icons.whatshot, color: Colors.orange)
+                : Icon(Icons.whatshot, color: Colors.grey[700]),
+            SizedBox(width: 4.0),
+            Text(
+              '$_contadorFuego',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Spacer(),
+            Icon(Icons.attach_money),
+            SizedBox(width: 4.0),
+            Text(
+              '0',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
           ],
         ),
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(48.0), // Altura deseada de la segunda barra
+          preferredSize: Size.fromHeight(48.0),
           child: AppBar(
-            title: Text('Tema seleccionado'), // Título de la segunda barra
-
-            automaticallyImplyLeading: false, // Oculta el botón de retorno
-            backgroundColor: Colors.grey[200], // Color de fondo de la segunda barra
+            title: Text('Tema seleccionado', style: TextStyle(color: Colors.black)), // Cambia el color aquí
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.grey[200],
           ),
         ),
       ),
@@ -69,12 +129,9 @@ class PaginaPrincipal extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // Apartado 1
             _buildSection("Apartado 1", Colors.blue, context),
             SizedBox(height: 16.0),
-            // Apartado 2
             _buildSection("Apartado 2", Colors.green, context),
-            // Puedes añadir más apartados según sea necesario
           ],
         ),
       ),
@@ -100,12 +157,14 @@ class PaginaPrincipal extends StatelessWidget {
             ),
           ),
           SizedBox(height: 8.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Column(
             children: [
               _buildLessonButton(Icons.edit, context),
+              SizedBox(height: 16),
               _buildLessonButton(Icons.edit, context),
+              SizedBox(height: 16),
               _buildLessonButton(Icons.edit, context),
+              SizedBox(height: 16),
               _buildLessonButton(Icons.edit, context),
             ],
           ),
@@ -125,8 +184,71 @@ class PaginaPrincipal extends StatelessWidget {
       child: IconButton(
         icon: Icon(iconData),
         onPressed: () {
-          // Acción al presionar el botón de la lección (puedes navegar a la página de la lección)
+          // Acción al presionar el botón de la lección
         },
+      ),
+    );
+  }
+}
+
+class SeleccionCurso extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selecciona un curso'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ExpansionTile(
+            title: const Text('Matemáticas'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('Algebra lineal'),
+                onTap: () {
+                  Navigator.pop(context, 'Algebra lineal');
+                },
+              ),
+              ListTile(
+                title: const Text('Geometría'),
+                onTap: () {
+                  Navigator.pop(context, 'Geometría');
+                },
+              ),
+              ListTile(
+                title: const Text('Cálculo 1'),
+                onTap: () {
+                  Navigator.pop(context, 'Cálculo 1');
+                },
+              ),
+              ListTile(
+                title: const Text('Cálculo 2'),
+                onTap: () {
+                  Navigator.pop(context, 'Cálculo 2');
+                },
+              ),
+              ListTile(
+                title: const Text('...'),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Ciencias'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('...'),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Programación'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('...'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
